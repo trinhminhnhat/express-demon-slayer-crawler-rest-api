ad088fce8999c08b3a2bc7b0e05411fbef3f3b3a
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2022-11-13 18:20:27 +0700

    feat: implement api get list characters in Demon Slayer

--- index.js
+++ index.js
@@ -26,9 +26,64 @@ app.use(
     }),
 );
 
+// routes
 app.get('/', (req, res) => {
-    return res.json({
-        name: 'app',
+    return res.status(200).json({
+        title: 'Welcome to Demon Slayer API',
+        api_links: {
+            get_all_characters: `${process.env.HOST_URL}/characters`,
+            character_detail: `${process.env.HOST_URL}/characters/:name`,
+        },
+    });
+});
+
+app.get('/characters', (req, res) => {
+    try {
+        const data = [];
+        const limit = Number(req.query.limit);
+        const url = process.env.WEB_CRAWL_URL + '/Kimetsu_no_Yaiba_Wiki';
+
+        axios(url)
+            .then((response) => {
+                const html = response.data;
+                const $ = cheerio.load(html);
+
+                $('.wds-tabber .portal').each(function () {
+                    const name = $(this).find('a').attr('title');
+                    const url = $(this).find('a').attr('href');
+                    const image = $(this).find('a > img').attr('data-src');
+
+                    data.push({
+                        name,
+                        image,
+                        detail_url: process.env.HOST_URL + '/character' + url.split('/wiki')[1],
+                    });
+                });
+
+                if (limit && limit > 0) {
+                    data = data.slice(0, limit);
+                }
+
+                return res.status(200).json({
+                    status: 200,
+                    data,
+                });
+            })
+            .catch((err) => {
+                return res.status(500).json({
+                    status: 500,
+                    message: 'API error: ' + err.message,
+                });
+            });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: 404,
+        message: 'Not Found',
     });
 });
 
